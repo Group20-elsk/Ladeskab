285800349675c504cad169b5ac2cd535c1c2df19
Patricia Danielsen <au599213@post.au.dk>
2020-03-04 15:51:24 +0100

    Tilføjet kode i ChargeControl

--- LadeskabLogik/ChargeControl.cs
+++ LadeskabLogik/ChargeControl.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace LadeskabLogik
 {
-    public class ChargeControl
+    public class ChargeControl : IChangeControl
     {
+        public double CurrentCurrent { get; set; }
+        private IDisplay _display;
+
+        public ChargeControl(IUsbCharger usbCharger, IDisplay display)
+        {
+            _display = display;
+            usbCharger.CurrentValueEvent += HandleCurrentEvent;
+        }
+
+        public void HandleCurrentEvent(object sender, CurrentEventArgs e)
+        {
+            CurrentCurrent = e.Current;
+            //Muligvis tilføj mere. 
+        }
     }
 }
