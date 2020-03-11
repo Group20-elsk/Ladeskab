69fb203cf31a4ea0d684db41c4d6a5251eceb389
Mie Tollund Sørensen <au586191@uni.au.dk>
2020-03-11 15:29:12 +0100

    Console ændringer for door-funktion

--- Ladeskab/Program.cs
+++ Ladeskab/Program.cs
@@ -15,18 +15,18 @@ namespace LadeskabAPP
             Console.WriteLine("To close the door, press C");
             
            IDoor door = new Door();
-           //bool doorOpen;
+           
 
             var key = Console.ReadKey(true);//Skal altid læse hvad for en tast der bliver tastet på
-            //bool doorOpen;
+            bool doorOpen;
             switch (key.KeyChar)
             {
                 case 'O':
-                    
+                    door.SetDoorStatus(doorOpen = true);
                     break;
 
                 case 'C':
-                    
+                    door.SetDoorStatus(doorOpen = false);
                     break;
 
                 
