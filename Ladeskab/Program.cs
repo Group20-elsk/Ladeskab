e398bf676685ecf70f87abeb67a761654a10e55b
Mie Tollund Sørensen <au586191@uni.au.dk>
2020-03-11 15:00:59 +0100

    Funktionen til at åbne og lukke døren

--- Ladeskab/Program.cs
+++ Ladeskab/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LadeskabLogik;
 
 namespace LadeskabAPP
 {
@@ -10,18 +11,21 @@ namespace LadeskabAPP
     {
         static void Main(string[] args)
         {
-            console.writeline("To open the door, press O");
-            console.writeline("To close the door, press C");
+           Console.WriteLine("To open the door, press O");
+           Console.WriteLine("To close the door, press C");
             
+           IDoor door = new Door();
+           bool doorOpen;
+
             var key = Console.ReadKey(true);//Skal altid læse hvad for en tast der bliver tastet på
             switch (key.KeyChar)
             {
                 case 'O':
-                    
+                    door.SetDoorStatus(doorOpen = true);
                     break;
 
                 case 'C':
-                    
+                    door.SetDoorStatus(doorOpen = false);
                     break;
 
                 
