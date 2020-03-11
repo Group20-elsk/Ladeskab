853625a8ebc98d9aebace5de0361b45467da967e
Kia Boesen Thomsen <201805069@post.au.dk>
2020-03-11 15:17:26 +0100

    Enter

--- Ladeskab/Program.cs
+++ Ladeskab/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LadeskabLogik;
 
 namespace LadeskabAPP
 {
@@ -10,18 +11,18 @@ namespace LadeskabAPP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("To open the door, press O");
-            Console.WriteLine("To close the door, press C");
+            console.writeline("To open the door, press O");
+            console.writeline("To close the door, press C");
             
             var key = Console.ReadKey(true);//Skal altid læse hvad for en tast der bliver tastet på
             switch (key.KeyChar)
             {
                 case 'O':
-                    
+                    door.SetDoorStatus(doorOpen = true);
                     break;
 
                 case 'C':
-                    
+                    door.SetDoorStatus(doorOpen = false);
                     break;
 
                 
