e34836a16d6bc5c71e268814b163380ad057818e
Patricia Danielsen <au599213@post.au.dk>
2020-03-17 14:48:57 +0100

    Oprettet test klasse for ChargeControl

---
+++ LadeskabUnitTest/TestChargeControl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LadeskabLogik;
+using NUnit.Framework;
+
+namespace LadeskabUnitTest
+{
+    [TestFixture]
+    public class TestChargeControl
+    {
+        private ChargeControl _uut;
+        private IUsbCharger usbCharger;
+        private IDisplay display; 
+
+        [SetUp]
+        public void Setup()
+        {
+            _uut = new ChargeControl(usbCharger, display); 
+        }
+
+
+    }
+}
