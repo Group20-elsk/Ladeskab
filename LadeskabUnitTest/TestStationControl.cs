0854e8bcd772177c875ecf5b3d33f52c7c4ce532
Mie Tollund Sørensen <au586191@uni.au.dk>
2020-04-01 17:08:08 +0200

    Ændringer i TestStationControl

--- LadeskabUnitTest/TestStationControl.cs
+++ LadeskabUnitTest/TestStationControl.cs
@@ -30,41 +30,63 @@ namespace LadeskabUnitTest
             _display = Substitute.For<IDisplay>();
             _uut = new StationControl(_door,_rfidReader,_log,_chargeControl,_display);
         }
-
-
-
-
-
-        [TestCase(true)]
-        [TestCase(false)]
-        public void TestDoorStatus(bool doorStatus) //Interaction-based test
+        [Test]
+        public void RaisedDoorChangeEvent_True_DoorOpen_Received_tilslut_telefon()
         {
-            _door.SetDoorStatus(doorStatus);
-            Assert.That(_uut.CurrentDoorStatus, Is.EqualTo(doorStatus));
+            _door.DoorChangedEvents += Raise.EventWith(new DoorChangedEventArgs(){DoorStatus = true});
+            _display.Received().writeDisplay("Tilslut telefon");
+        }
+        [Test]
+        public void RaisedDoorChangeEvent_False_DoorOpen_NotReceived_tilslut_telefon()
+        {
+            _door.DoorChangedEvents += Raise.EventWith(new DoorChangedEventArgs() { DoorStatus = false });
+            _display.DidNotReceive().writeDisplay("Tilslut telefon");
         }
 
-        [TestCase(true)]
-        [TestCase(false)]
-        public void TestCurrentDoorStatus(bool doorStatus) //Value-based test
+        [Test]
+        public void RaisedDoorChangeEvent_False_Available_Received_Indlæs_RFID()
         {
-            _uut.CurrentDoorStatus = doorStatus;
-            Assert.That(_uut.CurrentDoorStatus, Is.EqualTo(doorStatus));
+            _door.DoorChangedEvents += Raise.EventWith(new DoorChangedEventArgs() { DoorStatus = false });
+            _display.Received().writeDisplay("Indlæs RFID");
+        }
+        [Test]
+        public void RaisedDoorChangeEvent_True_Available_NotReceived_Indlæs_RFID()
+        {
+            _door.DoorChangedEvents += Raise.EventWith(new DoorChangedEventArgs() { DoorStatus = true });
+            _display.DidNotReceive().writeDisplay("Indlæs RFID");
         }
 
-        [TestCase(true)]
-        [TestCase(false)]
-        public void TestRFIDreaderStatus(bool RFIDstatus) //Interaction-based test
-        {
-            _rfidReader.SetRfidReaderStatus(RFIDstatus);
-            Assert.That(_uut.CurrentRfidSensedStatus, Is.EqualTo(RFIDstatus));
-        }
 
-        [TestCase(true)]
-        [TestCase(false)]
-        public void TestCurrentRFIDStatus(bool RFIDStatus) //Value-based test
-        {
-            _uut.CurrentRfidSensedStatus = RFIDStatus;
-            Assert.That(_uut.CurrentRfidSensedStatus, Is.EqualTo(RFIDStatus));
-        }
+        //[TestCase(true)]
+        //[TestCase(false)]
+        //public void TestDoorStatus(bool doorStatus) //Interaction-based test
+        //{
+        //    _door.SetDoorStatus(doorStatus);
+        //    Assert.That(_uut.CurrentDoorStatus, Is.EqualTo(doorStatus));
+        //}
+
+        //[TestCase(true)]
+        //[TestCase(false)]
+        //public void TestCurrentDoorStatus(bool doorStatus) //Value-based test
+        //{
+        //    _uut.CurrentDoorStatus = doorStatus;
+        //    Assert.That(_uut.CurrentDoorStatus, Is.EqualTo(doorStatus));
+        //}
+
+        //[TestCase(true)]
+        //[TestCase(false)]
+        //public void TestRFIDreaderStatus(bool RFIDstatus) //Interaction-based test
+        //{
+        //    _rfidReader.SetRfidReaderStatus(RFIDstatus);
+        //    Assert.That(_uut.CurrentRfidSensedStatus, Is.EqualTo(RFIDstatus));
+        //}
+
+        //[TestCase(true)]
+        //[TestCase(false)]
+        //public void TestCurrentRFIDStatus(bool RFIDStatus) //Value-based test
+        //{
+        //    _uut.CurrentRfidSensedStatus = RFIDStatus;
+        //    Assert.That(_uut.CurrentRfidSensedStatus, Is.EqualTo(RFIDStatus));
+        //}
     }
 }
